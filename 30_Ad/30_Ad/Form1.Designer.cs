f4a58ecf3c00490f03adfcace53c8f4a9073af01
SorbanElod <92728622+SorbanElod@users.noreply.github.com>
2022-01-10 11:47:00 +0200

    Ad Started
    Not Finished

---
+++ 30_Ad/30_Ad/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace _30_Ad
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.label1 = new System.Windows.Forms.Label();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.BackColor = System.Drawing.SystemColors.ControlDark;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 17.8F);
+			this.label1.Location = new System.Drawing.Point(41, 26);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(500, 200);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "label1";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// timer1
+			// 
+			this.timer1.Interval = 10;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(582, 253);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Timer timer1;
+	}
+}
+
---
+++ 30_Ad/30_Ad/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _30_Ad
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			timer1.Interval = 1;
+			timer1.Start();
+		}
+		private void colour()
+		{
+			Random random = new Random();
+			label1.BackColor = Color.FromArgb(random.Next(255), random.Next(255), random.Next(255));
+			label1.ForeColor = Color.FromArgb(random.Next(255), random.Next(255), random.Next(255));
+			this.BackColor = Color.FromArgb(random.Next(255), random.Next(255), random.Next(255));
+		}
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			colour();
+		}
+	}
+}
