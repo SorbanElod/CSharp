2dcd2c3c4ad4228300a3baec28d1947dc34c0c72
SorbanElod <92728622+SorbanElod@users.noreply.github.com>
2022-01-12 12:27:05 +0200

    UPDATE

---
+++ 32_LabelsMoving/32_LabelsMoving/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace _32_LabelsMoving
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(50, 50);
+			this.label1.TabIndex = 0;
+			// 
+			// label2
+			// 
+			this.label2.BackColor = System.Drawing.Color.Lime;
+			this.label2.Location = new System.Drawing.Point(438, 411);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(50, 50);
+			this.label2.TabIndex = 1;
+			// 
+			// timer1
+			// 
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(500, 470);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Timer timer1;
+	}
+}
+
---
+++ 32_LabelsMoving/32_LabelsMoving/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _32_LabelsMoving
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		private int speed = 2;
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			move1();
+			move2();
+
+		}
+		private void move1()
+		{
+			int dirX = 1;
+			int dirY = 1;
+			int x = label1.Left + dirX * speed;
+			int y = label1.Top + dirX * speed;
+			if (x > 0 && x < ClientSize.Width - label1.Width)
+			{
+				label1.Left = x;
+			}
+			else
+			{
+				label1.Top = ClientSize.Height - label1.Height;
+				label1.Left = ClientSize.Width - label1.Width;
+			}
+			if (y > 0 && y < ClientSize.Height - label1.Height)
+			{
+				label1.Top = y;
+			}
+		}
+
+		private void move2()
+		{
+			int dirX = -1;
+			int dirY = -1;
+			int x = label2.Left + dirX * speed;
+			int y = label2.Top + dirY * speed;
+			if (x > 0 && x < ClientSize.Width - label2.Width)
+			{
+				label2.Left = x;
+			}
+			else
+			{
+				label2.Top = 0;
+				label2.Left = 0;
+			}
+			if (y > 0 && y < ClientSize.Height - label2.Height)
+			{
+				label2.Top = y;
+			}
+		}
+
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			label1.Top = 0;
+			label1.Left = 0;
+			label2.Top = ClientSize.Height - label2.Height;
+			label2.Left = ClientSize.Width - label2.Width;
+			timer1.Interval = 10;
+			timer1.Start();
+		}
+	}
+}
