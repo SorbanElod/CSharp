cb9f92fbbb9aa74384fa49bbc562cf91fb57bd8a
SorbanElod <92728622+SorbanElod@users.noreply.github.com>
2022-01-16 14:15:22 +0200

    PictureBoxPrograms

--- 34_Smiley/34_Smiley/Form1.Designer.cs
+++ 34_Smiley/34_Smiley/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace _33_Gym
+﻿namespace _34_Smiley
 {
 	partial class Form1
 	{
@@ -29,59 +29,43 @@
 		private void InitializeComponent()
 		{
 			this.components = new System.ComponentModel.Container();
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-			this.pictureBox1 = new System.Windows.Forms.PictureBox();
-			this.pictureBox2 = new System.Windows.Forms.PictureBox();
 			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
 			this.SuspendLayout();
-			// 
-			// pictureBox1
-			// 
-			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-			this.pictureBox1.Location = new System.Drawing.Point(12, 138);
-			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(249, 257);
-			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-			this.pictureBox1.TabIndex = 0;
-			this.pictureBox1.TabStop = false;
-			// 
-			// pictureBox2
-			// 
-			this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-			this.pictureBox2.Location = new System.Drawing.Point(-1, 1);
-			this.pictureBox2.Name = "pictureBox2";
-			this.pictureBox2.Size = new System.Drawing.Size(130, 278);
-			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-			this.pictureBox2.TabIndex = 1;
-			this.pictureBox2.TabStop = false;
 			// 
 			// timer1
 			// 
 			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
 			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = global::_34_Smiley.Properties.Resources.fejecs1;
+			this.pictureBox1.Location = new System.Drawing.Point(219, 49);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(362, 352);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(252, 450);
-			this.Controls.Add(this.pictureBox2);
+			this.ClientSize = new System.Drawing.Size(800, 450);
 			this.Controls.Add(this.pictureBox1);
 			this.Name = "Form1";
 			this.Text = "Form1";
 			this.Load += new System.EventHandler(this.Form1_Load);
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
 
-		private System.Windows.Forms.PictureBox pictureBox1;
-		private System.Windows.Forms.PictureBox pictureBox2;
 		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.PictureBox pictureBox1;
 	}
 }
 
--- 34_Smiley/34_Smiley/Form1.cs
+++ 34_Smiley/34_Smiley/Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace _33_Gym
+namespace _34_Smiley
 {
 	public partial class Form1 : Form
 	{
@@ -16,26 +16,17 @@ namespace _33_Gym
 		{
 			InitializeComponent();
 		}
-
+		private int i = 1;
 		private void Form1_Load(object sender, EventArgs e)
 		{
-			pictureBox1.Size = ClientSize;
-			pictureBox1.Visible = true;
-			pictureBox1.Top = 0;
-			pictureBox1.Left = 0;
-			pictureBox2.Top = 0;
-			pictureBox2.Left = 0;
-			pictureBox2.Size = ClientSize;
-			pictureBox2.Visible = false;
-			timer1.Interval = 100;
+			timer1.Interval = 1;
 			timer1.Start();
 		}
-		private bool tick = false;
+
 		private void timer1_Tick(object sender, EventArgs e)
 		{
-			pictureBox1.Visible = tick;
-			pictureBox2.Visible = !tick;
-			tick = !tick;
+			i = i % 4;
+			pictureBox1.Image = Bitmap.FromFile("fejecs" + ++i + ".bmp");
 		}
 	}
 }
