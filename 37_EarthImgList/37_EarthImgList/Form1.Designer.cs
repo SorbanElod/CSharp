40488bb32b40ffb9be59155e0a9e4cf15a615702
SorbanElod <92728622+SorbanElod@users.noreply.github.com>
2022-01-18 08:15:52 +0200

    Earth

---
+++ 37_EarthImgList/37_EarthImgList/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace _37_EarthImgList
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.button1 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// imageList1
+			// 
+			this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+			this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+			this.imageList1.Images.SetKeyName(0, "W1.png");
+			this.imageList1.Images.SetKeyName(1, "W2.png");
+			this.imageList1.Images.SetKeyName(2, "W3.png");
+			this.imageList1.Images.SetKeyName(3, "W4.png");
+			this.imageList1.Images.SetKeyName(4, "W5.png");
+			this.imageList1.Images.SetKeyName(5, "W6.png");
+			this.imageList1.Images.SetKeyName(6, "W7.png");
+			this.imageList1.Images.SetKeyName(7, "W8.png");
+			this.imageList1.Images.SetKeyName(8, "W9.png");
+			this.imageList1.Images.SetKeyName(9, "W10.png");
+			this.imageList1.Images.SetKeyName(10, "W11.png");
+			this.imageList1.Images.SetKeyName(11, "W12.png");
+			this.imageList1.Images.SetKeyName(12, "W13.png");
+			this.imageList1.Images.SetKeyName(13, "W14.png");
+			this.imageList1.Images.SetKeyName(14, "W15.png");
+			this.imageList1.Images.SetKeyName(15, "W16.png");
+			// 
+			// timer1
+			// 
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(96, 89);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(175, 166);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(524, 155);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(138, 81);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Start";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.pictureBox1);
+			this.Name = "Form1";
+			this.Text = "Earth";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ImageList imageList1;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button button1;
+	}
+}
+
---
+++ 37_EarthImgList/37_EarthImgList/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _37_EarthImgList
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			timer1.Start();
+		}
+		private int i = 0;
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			i = i % 16;
+			pictureBox1.Image = imageList1.Images[i++];
+		}
+	}
+}
