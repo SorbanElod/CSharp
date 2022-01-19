005c5b8535bd83d78782ad558bf4e270a8593d9f
SorbanElod <92728622+SorbanElod@users.noreply.github.com>
2022-01-19 12:42:01 +0200

    ButtonGenerator

---
+++ 38_ButtonGenerate/38_ButtonGenerate/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace _38_ButtonGenerate
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.button1 = new System.Windows.Forms.Button();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(556, 104);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(121, 49);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Generate";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// timer1
+			// 
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// panel1
+			// 
+			this.panel1.Location = new System.Drawing.Point(12, 12);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(522, 411);
+			this.panel1.TabIndex = 1;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Panel panel1;
+	}
+}
+
---
+++ 38_ButtonGenerate/38_ButtonGenerate/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _38_ButtonGenerate
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		static int n = 10;
+		Random r = new Random();
+		Button[] g = new Button[n];
+		private void button1_Click(object sender, EventArgs e)
+		{
+			timer1.Enabled = true;
+			// for (int i = 0; i < n; i++) panel1.Controls.Remove(g[i]);
+			for (int i = 0; i < n; i++)
+				if (g[i] != null) g[i].Dispose();
+			for (int i = 0; i < n; i++)
+			{
+				g[i] = new Button();
+				g[i].Name = "btn" + i;
+				g[i].Text = i.ToString();
+				g[i].Width = 40;
+				g[i].Height = 20;
+				g[i].Top = r.Next(panel1.Height - g[i].Height);
+				g[i].Left = r.Next(panel1.Width - g[i].Width);
+				panel1.Controls.Add(g[i]);
+				g[i].Click += new EventHandler(g_Click);
+			}
+		}
+		private void g_Click(object sender, EventArgs e)
+		{
+			//panel1.Controls.Remove(sender as Button);
+			(sender as Button).Dispose();
+		}
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			for (int i = 0; i < n; i++)
+			{
+
+				g[i].Top = r.Next(panel1.Height - g[i].Height);
+				g[i].Left = r.Next(panel1.Width - g[i].Width);
+
+			}
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			timer1.Interval = 500;
+		}
+	}
+}
