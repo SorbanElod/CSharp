60962f5c953a3437a12b145bf2c8426a8ef57f8d
SorbanElod <92728622+SorbanElod@users.noreply.github.com>
2022-01-21 10:10:47 +0200

    UPDATE

---
+++ 39_DividePanel/39_DividePanel/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace _39_DividePanel
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.buttonDelete = new System.Windows.Forms.Button();
+			this.buttonGenerate = new System.Windows.Forms.Button();
+			this.buttonExit = new System.Windows.Forms.Button();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.labelPrint = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// buttonDelete
+			// 
+			this.buttonDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.buttonDelete.Location = new System.Drawing.Point(33, 327);
+			this.buttonDelete.Name = "buttonDelete";
+			this.buttonDelete.Size = new System.Drawing.Size(97, 37);
+			this.buttonDelete.TabIndex = 0;
+			this.buttonDelete.Text = "Delete";
+			this.buttonDelete.UseVisualStyleBackColor = true;
+			this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+			// 
+			// buttonGenerate
+			// 
+			this.buttonGenerate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.buttonGenerate.Location = new System.Drawing.Point(161, 327);
+			this.buttonGenerate.Name = "buttonGenerate";
+			this.buttonGenerate.Size = new System.Drawing.Size(97, 37);
+			this.buttonGenerate.TabIndex = 1;
+			this.buttonGenerate.Text = "Generate";
+			this.buttonGenerate.UseVisualStyleBackColor = true;
+			this.buttonGenerate.Click += new System.EventHandler(this.buttonGenerate_Click);
+			// 
+			// buttonExit
+			// 
+			this.buttonExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.buttonExit.Location = new System.Drawing.Point(33, 386);
+			this.buttonExit.Name = "buttonExit";
+			this.buttonExit.Size = new System.Drawing.Size(97, 37);
+			this.buttonExit.TabIndex = 2;
+			this.buttonExit.Text = "Exit";
+			this.buttonExit.UseVisualStyleBackColor = true;
+			this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.numericUpDown1.Location = new System.Drawing.Point(304, 327);
+			this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+			this.numericUpDown1.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(64, 26);
+			this.numericUpDown1.TabIndex = 3;
+			this.numericUpDown1.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+			// 
+			// panel1
+			// 
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(788, 303);
+			this.panel1.TabIndex = 4;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+			this.label1.Location = new System.Drawing.Point(512, 339);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(179, 20);
+			this.label1.TabIndex = 5;
+			this.label1.Text = "Pressed Button Number";
+			// 
+			// labelPrint
+			// 
+			this.labelPrint.AutoSize = true;
+			this.labelPrint.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+			this.labelPrint.Location = new System.Drawing.Point(512, 394);
+			this.labelPrint.Name = "labelPrint";
+			this.labelPrint.Size = new System.Drawing.Size(18, 20);
+			this.labelPrint.TabIndex = 6;
+			this.labelPrint.Text = "0";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.labelPrint);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.numericUpDown1);
+			this.Controls.Add(this.buttonExit);
+			this.Controls.Add(this.buttonGenerate);
+			this.Controls.Add(this.buttonDelete);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button buttonDelete;
+		private System.Windows.Forms.Button buttonGenerate;
+		private System.Windows.Forms.Button buttonExit;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label labelPrint;
+	}
+}
+
---
+++ 39_DividePanel/39_DividePanel/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _39_DividePanel
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		static int n = 10;
+		private void buttonExit_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+		private void buttonDelete_Click(object sender, EventArgs e)
+		{
+			panel1.Controls.Clear();
+			buttonGenerate.Enabled = true;
+			buttonDelete.Enabled = false;
+		}
+		Random rnGee = new Random();
+		private void buttonGenerate_Click(object sender, EventArgs e)
+		{
+			n = (int)(numericUpDown1.Value);
+			Label[] g = new Label[n];
+			for(int i = 0; i < n; i++)
+			{
+				g[i] = new Label();
+				g[i].Name = "lb" + i;
+				g[i].Size = panel1.Size;
+				g[i].Width /= n;
+				g[i].Left = ((panel1.Width / n) * i);
+				g[i].BackColor = Color.FromArgb(rnGee.Next(0, 255), rnGee.Next(0, 255), rnGee.Next(0, 255));
+				g[i].Click += new EventHandler(g_Click);
+				g[i].MouseEnter += new EventHandler(g_Click);
+				panel1.Controls.Add(g[i]);
+				buttonGenerate.Enabled = false;
+				buttonDelete.Enabled = true;
+			}
+		}
+		private void g_Click(object sender, EventArgs e)
+		{
+			(sender as Label).BackColor = Color.FromArgb(rnGee.Next(0, 255), rnGee.Next(0, 255), rnGee.Next(0, 255));
+			labelPrint.Text = ((sender as Label).Left/ (sender as Label).Width).ToString();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			buttonDelete.Enabled = false;
+		}
+	}
+}
