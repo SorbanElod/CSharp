9e93576a211dbba2bd9d7ad12fd3ddc479dfe5f9
SorbanElod <92728622+SorbanElod@users.noreply.github.com>
2022-01-25 06:42:58 +0200

    PuzzleSort

---
+++ 41_PuzzleSort/41_PuzzleSort/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace _41_PuzzleSort
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.buttonClear = new System.Windows.Forms.Button();
+			this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+			this.buttonCreate = new System.Windows.Forms.Button();
+			this.buttonExit = new System.Windows.Forms.Button();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(800, 228);
+			this.panel1.TabIndex = 0;
+			// 
+			// buttonClear
+			// 
+			this.buttonClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+			this.buttonClear.Location = new System.Drawing.Point(53, 284);
+			this.buttonClear.Name = "buttonClear";
+			this.buttonClear.Size = new System.Drawing.Size(94, 45);
+			this.buttonClear.TabIndex = 1;
+			this.buttonClear.Text = "Clear";
+			this.buttonClear.UseVisualStyleBackColor = true;
+			this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+			// 
+			// fileSystemWatcher1
+			// 
+			this.fileSystemWatcher1.EnableRaisingEvents = true;
+			this.fileSystemWatcher1.SynchronizingObject = this;
+			// 
+			// buttonCreate
+			// 
+			this.buttonCreate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+			this.buttonCreate.Location = new System.Drawing.Point(179, 284);
+			this.buttonCreate.Name = "buttonCreate";
+			this.buttonCreate.Size = new System.Drawing.Size(94, 45);
+			this.buttonCreate.TabIndex = 2;
+			this.buttonCreate.Text = "Create";
+			this.buttonCreate.UseVisualStyleBackColor = true;
+			this.buttonCreate.Click += new System.EventHandler(this.buttonCreate_Click);
+			// 
+			// buttonExit
+			// 
+			this.buttonExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+			this.buttonExit.Location = new System.Drawing.Point(676, 284);
+			this.buttonExit.Name = "buttonExit";
+			this.buttonExit.Size = new System.Drawing.Size(94, 45);
+			this.buttonExit.TabIndex = 3;
+			this.buttonExit.Text = "Exit";
+			this.buttonExit.UseVisualStyleBackColor = true;
+			this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+			this.numericUpDown1.Location = new System.Drawing.Point(316, 284);
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(94, 30);
+			this.numericUpDown1.TabIndex = 4;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.numericUpDown1);
+			this.Controls.Add(this.buttonExit);
+			this.Controls.Add(this.buttonCreate);
+			this.Controls.Add(this.buttonClear);
+			this.Controls.Add(this.panel1);
+			this.Name = "Form1";
+			this.Text = "PuzzleSort";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Button buttonClear;
+		private System.IO.FileSystemWatcher fileSystemWatcher1;
+		private System.Windows.Forms.Button buttonExit;
+		private System.Windows.Forms.Button buttonCreate;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+	}
+}
+
---
+++ 41_PuzzleSort/41_PuzzleSort/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _41_PuzzleSort
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		static int n = 100;
+		Button[] g = new Button[n];
+		static int current = n-1;
+		private void buttonExit_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+		private void buttonClear_Click(object sender, EventArgs e)
+		{
+			for (int i = 0; i < n; i++)
+			{
+				if (g[i] != null)
+				{
+					g[i].Dispose();
+				}
+			}
+			//panel1.Controls.Clear();
+			buttonCreate.Enabled = true;
+			buttonClear.Enabled = false;
+		}
+		Random rnGee = new Random();
+		private void buttonCreate_Click(object sender, EventArgs e)
+		{
+			buttonCreate.Enabled = false;
+			buttonClear.Enabled = true;
+
+			n = (int)(numericUpDown1.Value);
+			current = n - 1;
+			for (int i = 0; i < n; i++)
+			{
+				g[i] = new Button();
+					g[i].Name = "lb" + i;
+					g[i].Text = rnGee.Next(1,999).ToString();
+					g[i].Size = panel1.Size;
+					g[i].Width /= n;
+					g[i].Left = (g[i].Width * i);
+					g[i].Click += new EventHandler(g_Click);
+					panel1.Controls.Add(g[i]);
+			}
+			g[n-1].Text = "";
+			current = n - 1;
+		}
+		private void g_Click(object sender, EventArgs e)
+		{
+			g[current].Text = (sender as Button).Text;
+			(sender as Button).Text = "";
+			current = (sender as Button).Left / (sender as Button).Width;
+			if(Check() == true)
+			{
+				MessageBox.Show("Good");
+			}
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			buttonCreate.Enabled = true;
+			buttonClear.Enabled = false;
+			numericUpDown1.Maximum = n;
+		}
+		private bool Check()
+		{
+			bool buul = false;
+			if(g[0].Text == "")
+			{
+				buul = true;
+				for(int i = 1; i < n-1; i++)
+				{
+					if (int.Parse(g[i].Text) > int.Parse(g[i + 1].Text)) ;
+					buul = true;
+				}
+			}
+			return buul;
+		}
+	}
+}
