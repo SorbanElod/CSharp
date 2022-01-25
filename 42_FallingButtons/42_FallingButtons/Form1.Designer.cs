d111d45f5795023897c92608e1b6dd7c7a73c44b
SorbanElod <92728622+SorbanElod@users.noreply.github.com>
2022-01-25 07:31:53 +0200

    FallingButton

---
+++ 42_FallingButtons/42_FallingButtons/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace _42_FallingButtons
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.buttonStart = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.Chartreuse;
+			this.panel1.Location = new System.Drawing.Point(12, 12);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(491, 510);
+			this.panel1.TabIndex = 0;
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+			this.numericUpDown1.Location = new System.Drawing.Point(637, 12);
+			this.numericUpDown1.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(98, 30);
+			this.numericUpDown1.TabIndex = 1;
+			this.numericUpDown1.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+			// 
+			// buttonStart
+			// 
+			this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+			this.buttonStart.Location = new System.Drawing.Point(637, 63);
+			this.buttonStart.Name = "buttonStart";
+			this.buttonStart.Size = new System.Drawing.Size(98, 45);
+			this.buttonStart.TabIndex = 2;
+			this.buttonStart.Text = "Start";
+			this.buttonStart.UseVisualStyleBackColor = true;
+			this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+			this.label1.Location = new System.Drawing.Point(637, 157);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(98, 45);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "0";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// timer1
+			// 
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.ForestGreen;
+			this.ClientSize = new System.Drawing.Size(769, 534);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.buttonStart);
+			this.Controls.Add(this.numericUpDown1);
+			this.Controls.Add(this.panel1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+		private System.Windows.Forms.Button buttonStart;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Timer timer1;
+	}
+}
+
---
+++ 42_FallingButtons/42_FallingButtons/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _42_FallingButtons
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		static int n = 16;
+		Random rnGee = new Random();
+		Button [] g = new Button [n]; 
+		private int index = 0;
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			timer1.Interval = 50;
+			numericUpDown1.Maximum = n;
+		}
+
+		private void buttonStart_Click(object sender, EventArgs e)
+		{
+			n = (int)(numericUpDown1.Value);
+			for(int i = 0; i < n; i++)
+			{
+				g[i] = new Button();
+				g[i].Text = (i+1).ToString();
+				g[i].Width = panel1.Width / n;
+				g[i].Left = g[i].Width * i;
+				g[i].Click += new System.EventHandler(g_Click);
+				g[i].BackColor = Color.FromArgb(rnGee.Next(0, 255), rnGee.Next(0, 255), rnGee.Next(0, 255));
+				panel1.Controls.Add(g[i]);
+				g[i].Visible = false;
+			}
+			int a = rnGee.Next(n);
+			g[a].Visible = true;
+			numericUpDown1.Enabled = false;
+			index = a;
+			if (timer1.Enabled == true)
+			{
+				timer1.Stop();
+				buttonStart.Text = "Start";
+			}
+			else
+			{
+				timer1.Start();
+				buttonStart.Text = "Stop";
+			}
+		}
+		private void g_Click(object sender, EventArgs e)
+		{
+			label1.Text = (int.Parse(label1.Text) + int.Parse((sender as Button).Text)).ToString();
+			(sender as Button).Visible = false;
+			int a = rnGee.Next(n);
+			g[a].Visible = true;
+			g[a].Top = 0;
+			index = a;
+		}
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			g[index].Top += 5;
+			if (g[index].Top >= panel1.Height - g[index].Height)
+			{
+				g[index].Visible = false;
+				int a = rnGee.Next(n);
+				g[a].Visible = true;
+				g[a].Top = 0;
+				index = a;
+			}
+		}
+		protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+		{
+			if (keyData == Keys.Enter)
+			{
+				return true;
+			}
+
+			return base.ProcessCmdKey(ref msg, keyData);
+		}
+	}
+}
