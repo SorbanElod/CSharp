dae08c82136be3e581cf837eaf7b4cf5505d3a9d
SorbanElod <92728622+SorbanElod@users.noreply.github.com>
2022-02-09 11:29:46 +0200

    Sheep++

--- 49_Sheep++/49_Sheep++/Form1.Designer.cs
+++ 49_Sheep++/49_Sheep++/Form1.Designer.cs
@@ -32,6 +32,7 @@
 			this.pictureBoxWolf = new System.Windows.Forms.PictureBox();
 			this.pictureBoxSheep = new System.Windows.Forms.PictureBox();
 			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.button1 = new System.Windows.Forms.Button();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBoxWolf)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBoxSheep)).BeginInit();
 			this.SuspendLayout();
@@ -60,7 +61,17 @@
 			// 
 			// timer1
 			// 
+			this.timer1.Interval = 1;
 			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(307, 39);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(129, 79);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
 			// 
 			// Form1
 			// 
@@ -68,11 +79,15 @@
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackgroundImage = global::_49_Sheep__.Properties.Resources.ret;
 			this.ClientSize = new System.Drawing.Size(1025, 510);
+			this.Controls.Add(this.button1);
 			this.Controls.Add(this.pictureBoxSheep);
 			this.Controls.Add(this.pictureBoxWolf);
+			this.DoubleBuffered = true;
 			this.Name = "Form1";
 			this.Text = "Sheep++";
 			this.Load += new System.EventHandler(this.Form1_Load);
+			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+			this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
 			((System.ComponentModel.ISupportInitialize)(this.pictureBoxWolf)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBoxSheep)).EndInit();
 			this.ResumeLayout(false);
@@ -84,6 +99,7 @@
 		private System.Windows.Forms.PictureBox pictureBoxWolf;
 		private System.Windows.Forms.PictureBox pictureBoxSheep;
 		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Button button1;
 	}
 }
 
--- 49_Sheep++/49_Sheep++/Form1.cs
+++ 49_Sheep++/49_Sheep++/Form1.cs
@@ -12,18 +12,166 @@ namespace _49_Sheep__
 {
 	public partial class Form1 : Form
 	{
+		bool shUp, shDown, shLeft, shRight, wUp, wDown, wLeft, wRight;
+
+		private void Form1_KeyUp(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.A)
+			{
+				wLeft = false;
+			}
+			if (e.KeyCode == Keys.D)
+			{
+				wRight = false;
+			}
+			if (e.KeyCode == Keys.S)
+			{
+				wDown = false;
+			}
+			if (e.KeyCode == Keys.W)
+			{
+				wUp = false;
+			}
+			if (e.KeyCode == Keys.Left)
+			{
+				shLeft = false;
+			}
+			if (e.KeyCode == Keys.Right)
+			{
+				shRight = false;
+			}
+			if (e.KeyCode == Keys.Down)
+			{
+				shDown = false;
+			}
+			if (e.KeyCode == Keys.Up)
+			{
+				shUp = false;
+			}
+		}
+
+		int move = 5;
+		private void Form1_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.A)
+			{
+				wLeft = true;
+			}
+			if (e.KeyCode == Keys.D)
+			{
+				wRight = true;
+			}
+			if (e.KeyCode == Keys.S)
+			{
+				wDown = true;
+			}
+			if(e.KeyCode == Keys.W)
+			{
+				wUp = true;
+			}
+			if(e.KeyCode == Keys.Left)
+			{
+				shLeft = true;
+			}
+			if(e.KeyCode== Keys.Right)
+			{
+				shRight = true;
+			}
+			if(e.KeyCode == Keys.Down)
+			{
+				shDown = true;
+			}
+			if(e.KeyCode == Keys.Up)
+			{
+				shUp = true;
+			}
+
+		}
+
 		public Form1()
 		{
 			InitializeComponent();
+
 		}
 		private void Form1_Load(object sender, EventArgs e)
 		{
-
+			timer1.Enabled = true;
+			foreach (Control control in this.Controls)
+			{
+				control.PreviewKeyDown += new PreviewKeyDownEventHandler(control_PreviewKeyDown);
+			}
+			this.KeyPreview = true;//hogy a form kapja el először a key-eventet
 		}
 
+		void control_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+		{
+			/*Some key presses, such as the TAB, RETURN, ESC, and arrow keys, are typically ignored by some controls
+             * because they are not considered input key presses. For example, by default, a Button control ignores 
+             * the arrow keys. Pressing the arrow keys typically causes the focus to move to the previous or next 
+             * control. The arrow keys are considered navigation keys and pressing these keys typically do not raise
+             * the KeyDown event for aButton. However, pressing the arrow keys for a Button does raise the PreviewKeyDown event. 
+             * By handling the PreviewKeyDown event for a Button and setting the IsInputKey property to true, 
+             * you can raise the KeyDown event when the arrow keys are pressed. 
+             * However, if you handle the arrow keys, the focus will no longer move to the previous or next control.*/
+			if (e.KeyCode == Keys.Up || e.KeyCode == Keys.Down || e.KeyCode == Keys.Left || e.KeyCode == Keys.Right)
+			{
+				e.IsInputKey = true;//Gets or sets a value indicating whether a key is a regular input key.
+									//a fokust ne allitsa at
+			}
+		}
+		/* !important
+		bool ham(PictureBox p1,PictureBox p2)
+		{
+			double x01 = p1.Left + p1.Width / 2, y01 = p1.Top + p1.Height / 2, x02 = p2.Left + p2.Width / 2, y02 = p2.Top + p2.Height / 2;
+			double d = Math.Sqrt((x01 - x02) * (x01 - x02) + (y01 - y02) * (y01 - y02));
+			if(d<(p1.Width + p2.Width) / 2 - 20)
+			{
+				return true;
+			}
+			return false;
+		}
+		*/
 		private void timer1_Tick(object sender, EventArgs e)
 		{
-
+			if (pictureBoxWolf.Bounds.IntersectsWith(pictureBoxSheep.Bounds)|| pictureBoxSheep.Bounds.IntersectsWith(pictureBoxWolf.Bounds))
+			{
+				//pictureBoxSheep.Image = Image.FromFile("bari5.png");
+				pictureBoxSheep.Image.RotateFlip(RotateFlipType.Rotate180FlipX);
+				pictureBoxSheep.Refresh();
+				timer1.Enabled = false;
+			}
+			if (shUp)
+			{
+				pictureBoxSheep.Top -= move;
+			}
+			if (shDown)
+			{
+				pictureBoxSheep.Top += move;
+			}
+			if (shLeft)
+			{
+				pictureBoxSheep.Left -= move;
+			}
+			if (shRight)
+			{
+				pictureBoxSheep.Left += move;
+			}
+			if (wUp)
+			{
+				pictureBoxWolf.Top -= move;
+			}
+			if (wDown)
+			{
+				pictureBoxWolf.Top += move;
+			}
+			if (wLeft)
+			{
+				pictureBoxWolf.Left -= move;
+			}
+			if (wRight)
+			{
+				pictureBoxWolf.Left += move;
+			}
 		}
 	}
 }
