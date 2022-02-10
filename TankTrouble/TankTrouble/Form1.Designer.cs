8b1d65581ac7a330f0e82d7cf7007afc71cf1056
SorbanElod <92728622+SorbanElod@users.noreply.github.com>
2022-02-10 07:05:49 +0200

    TT1.0

---
+++ TankTrouble/TankTrouble/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace TankTrouble
+{
+	partial class TestPage
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.pTest = new System.Windows.Forms.Panel();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.pTest.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pTest
+			// 
+			this.pTest.Controls.Add(this.pictureBox1);
+			this.pTest.Location = new System.Drawing.Point(12, 12);
+			this.pTest.Name = "pTest";
+			this.pTest.Size = new System.Drawing.Size(776, 426);
+			this.pTest.TabIndex = 0;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Arrow;
+			this.pictureBox1.Image = global::TankTrouble.Properties.Resources.Tank;
+			this.pictureBox1.Location = new System.Drawing.Point(256, 56);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(200, 330);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// timer1
+			// 
+			this.timer1.Interval = 1;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// TestPage
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.pTest);
+			this.Name = "TestPage";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.TestPage_Load);
+			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TestPage_KeyDown);
+			this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.TestPage_KeyUp);
+			this.pTest.ResumeLayout(false);
+			this.pTest.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel pTest;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+	}
+}
+
---
+++ TankTrouble/TankTrouble/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TankTrouble
+{
+	public partial class TestPage : Form
+	{
+		public TestPage()
+		{
+			InitializeComponent();
+		}
+		bool tUp,tDown,tLeft, tRight;
+		int moveSpeed = 5;
+		float turnSpeed = 5;
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			if (tUp)
+			{
+				pictureBox1.Top -= moveSpeed;
+			}
+			if (tDown)
+			{
+				pictureBox1.Top += moveSpeed;
+			}
+			if (tLeft)
+			{
+				Image img = RotateImage2((Bitmap)pictureBox1.Image, 360 - turnSpeed);
+				//pictureBox1.Size = img.Size;
+				pictureBox1.Image = img;
+				
+			}
+			if (tRight)
+			{
+				Image img = RotateImage2((Bitmap)pictureBox1.Image, turnSpeed);
+				//pictureBox1.Size = img.Size;
+				pictureBox1.Image = img;
+			}
+		}
+
+		private void TestPage_KeyUp(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Left)
+			{
+				tLeft = false;
+			}
+			if (e.KeyCode == Keys.Right)
+			{
+				tRight = false;
+			}
+			if (e.KeyCode == Keys.Down)
+			{
+				tDown = false;
+			}
+			if (e.KeyCode == Keys.Up)
+			{
+				tUp = false;
+			}
+		}
+
+		private void TestPage_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Left)
+			{
+				tLeft = true;
+			}
+			if (e.KeyCode == Keys.Right)
+			{
+				tRight = true;
+			}
+			if (e.KeyCode == Keys.Down)
+			{
+				tDown = true;
+			}
+			if (e.KeyCode == Keys.Up)
+			{
+				tUp = true;
+			}
+		}
+
+		public static Bitmap RotateImage(Bitmap b, float angle)
+		{
+			//create a new empty bitmap to hold rotated image
+			Bitmap returnBitmap = new Bitmap(b.Width, b.Height);
+			//make a graphics object from the empty bitmap
+			using (Graphics g = Graphics.FromImage(returnBitmap))
+			{
+				//move rotation point to center of image
+				g.TranslateTransform((float)b.Width / 2, (float)b.Height / 2);
+				//rotate
+				g.RotateTransform(angle);
+				//move image back
+				g.TranslateTransform(-(float)b.Width / 2, -(float)b.Height / 2);
+				//draw passed in image onto graphics object
+				g.DrawImage(b, new Point(0, 0));
+			}
+			return returnBitmap;
+		}
+		private Bitmap RotateImage2(Bitmap bmp, float angle)
+		{
+			float height = bmp.Height;
+			float width = bmp.Width;
+			int hypotenuse = System.Convert.ToInt32(System.Math.Floor(Math.Sqrt(height * height + width * width)));
+			Bitmap rotatedImage = new Bitmap(hypotenuse, hypotenuse);
+			using (Graphics g = Graphics.FromImage(rotatedImage))
+			{
+				g.TranslateTransform((float)rotatedImage.Width / 2, (float)rotatedImage.Height / 2); //set the rotation point as the center into the matrix
+				g.RotateTransform(angle); //rotate
+				g.TranslateTransform(-(float)rotatedImage.Width / 2, -(float)rotatedImage.Height / 2); //restore rotation point into the matrix
+				g.DrawImage(bmp, (hypotenuse - width) / 2, (hypotenuse - height) / 2, width, height);
+			}
+			return rotatedImage;
+		}
+		private void TestPage_Load(object sender, EventArgs e)
+		{
+			timer1.Start();
+		}
+	}
+}
